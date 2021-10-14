f4f3953e040bc3fddd42bad23e668fab229304e5
zermiftahudin <alucardmiftah896@gmail.com>
2021-10-14 11:27:26 +0700

    ecommercejersey

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                Copyright @2021 Muhammad Miftahudin
+            </div>
+        </div>
+    </div>
+</footer>
